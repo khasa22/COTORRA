ffbe35607ca2115a2abaad3662bb4c569010d8cc
Jorge <jmartinp@it.uc3m.es>
2023-01-26 18:10:48 +0100

    Code the stressed networks

--- dlmd/stress-test.py
+++ dlmd/stress-test.py
@@ -4,9 +4,14 @@
 import functools as reduce
 import networkx as nx
 import pandas as pd
+import numpy as np
+import copy
 
+##################
+# DLMD FUNCTIONS #
+##################
 
-def dlmd(G, SFC, max_delay, PoAs):
+def dlmd(G, SFC, PoAs):
     # Executes de DLMD algorithm and yields the VF-placement for a robotic
     # service with a robot that can be attached to the PoAs. 
     # The code asumes the feasible PoAs are done.
@@ -43,6 +48,7 @@ def dlmd(G, SFC, max_delay, PoAs):
     for vf in vfs:
         min_eta = 1000
 
+        # TODO: add also the prior vfs deployed there
         for s in [s for s in servers\
                 if SFC.nodes[vf]['cpus'] <= G.nodes[s]['cpus']]:
             # Server cost
@@ -56,7 +62,7 @@ def dlmd(G, SFC, max_delay, PoAs):
                 # Prune links without enough BW
                 vl_bw = SFV.edges(data=True)[vls[hop]]
                 pruned_links = low_bw(G, vl_bw)
-                G_pruned = G.copy()
+                G_pruned = copy.deepcopy(G)
                 G_pruned.remove_edges_from(pruned_links)
 
                 # Find path between (vf,vf-1)
@@ -99,10 +105,94 @@ def deployment_delay(G, deployment):
     return delay
 
 
+def deployment_free_edge(G, SFC, deployment):
+    # Calculates the free edge resources at the edge 
+    # note: the deployment must have been successfull
+
+    n_edges = [n for n,d in G.nodes(data=True) if d['type']=='near_edge']
+    n_edge_cpus = sum([d['cpus'] for n,d in G.nodes(data=True)\
+                        if d['type']=='near_edge'])
+    f_edges = [n for n,d in G.nodes(data=True) if d['type']=='far_edge']
+    f_edge_cpus = sum([d['cpus'] for n,d in G.nodes(data=True)\
+                        if d['type']=='far_edge'])
+
+    # Total edge resource
+    total_edge = n_edge_cpus + f_edge_cpus
+    consumed_edge = n_edge_cpus + f_edge_cpus
+
+    for vf in SFC.nodes:
+        if G.nodes(data=True)[deployment[vf]]['type'] != 'cloud':
+            consumed_edge -= SFC.nodes(data=True)['cpus']
+            
+
+    return 1 - consumed_edge/total_edge
+    
+    
 
 
+
+
+#########################
+# STRESS-TEST FUNCTIONS #
+#########################
+
+def stress_network(G, p_step=0.1):
+    # Stress G resources at steps of p_step percent
+
+    stressed_networks = {0: copy.deepcopy(G_)}
+
+    # Get the server nodes
+    n_edges = [n for n,d in G.nodes(data=True) if d['type']=='near_edge']
+    f_edges = [n for n,d in G.nodes(data=True) if d['type']=='far_edge']
+    clouds = [n for n,d in G.nodes(data=True) if d['type']=='cloud']
+
+    # CPU sums
+    n_edge_cpus = G.nodes(data=True)[n_edges[0]]['cpus']
+    f_edge_cpus = G.nodes(data=True)[f_edges[0]]['cpus']
+    cloud_cpus = G.nodes(data=True)[clouds[0]]['cpus']
+
+    # CPU decreases and edge removal steps
+    edges_step = int(len(G.edges) * p_step)
+    n_edge_step = int(n_edge_cpus * p_step)
+    f_edge_step = int(f_edge_cpus * p_step)
+    cloud_step = int(cloud_cpus * p_step)
+
+    G_ = stressed_networks[0]
+    for p in np.arange(p_step, 1, p_step):
+
+        # Remove an edge from the highest degree node
+        for i in range(edges_step):
+            nodes_deg = sorted([(n, len(G_[n])) for n in G_.nodes],
+                           key=lambda x: x[1], reverse=True)
+            highest_neighs = 
+            G_.remove_edge(nodes_deg[0][0], list(G_[nodes_deg[0][0]])[0])
+
+        # Remove CPUs
+        for s in n_edges:
+            G_.nodes[s]['cpus'] = max(0, G_.nodes[s]['cpus'] - n_edge_step)
+        for s in f_edges:
+            G_.nodes[s]['cpus'] = max(0, G_.nodes[s]['cpus'] - f_edge_step)
+        for s in clouds:
+            G_.nodes[s]['cpus'] = max(0, G_.nodes[s]['cpus'] - cloud_step)
+
+        # Store the p-stressed graph
+        stressed_networks[p] = copy.deepcopy(G_)
+
+    return stressed_networks
 
 
 if __name__ == '__main__':
-    pass
+    # Create the SFC
+    SFC_len = 3
+    SFC = nx.path_graph(SFC_len)
+    VF_cpus = 1
+    nx.set_node_attributes(SFC, {n: VF_cpus for n in range(SFC_len)}, 'cpus')
+    print('SFC created ')
+
+    # Create the stressed networks
+    for G_ in stressed_networks:
+        dlmd()
+        dlmd(G_, SFC, PoAs)
+
+    # TODO: add to the DLMD the steering to the PoA
 
